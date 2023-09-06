7885336c0b3f7eda03795d3c8b2afcd5c03a81d8
alejandro-germosen <alejandro.germosen@solvx.com.do>
2023-09-06 13:23:40 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+## Description
+
+[Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
+
+## Stack
+
+* Nest
+* MongoDB
+
+## Installation
+
+1. Install dependencies
+```bash
+$ yarn install
+```
+
+2. Intall Nest CLI globally
+```bash
+$ npm i -g @nestjs/cli
+```
+
+3. Start database
+```bash
+$ docker-compose up -d
+```
+
+## Running the app
+
+```bash
+# development
+$ yarn run start
+
+# watch mode
+$ yarn run start:dev
+
+# production mode
+$ yarn run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ yarn run test
+
+# e2e tests
+$ yarn run test:e2e
+
+# test coverage
+$ yarn run test:cov
+```
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,97 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { Model, isValidObjectId } from 'mongoose';
+
+const MONGO_ERROR = {
+  DUPLICATE_KEY: 11000,
+};
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.sanitizedName;
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: term });
+      return pokemon;
+    }
+
+    if (isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+      return pokemon;
+    }
+
+    pokemon = await this.pokemonModel.findOne({
+      name: term.toLowerCase().trim(),
+    });
+
+    if (!pokemon) {
+      throw new NotFoundException(
+        `Pokemon could not be found using the term '${term}'`,
+      );
+    }
+
+    return pokemon;
+  }
+
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+
+    if (updatePokemonDto.name) {
+      updatePokemonDto.name = updatePokemonDto.sanitizedName;
+    }
+
+    try {
+      await pokemon.updateOne(updatePokemonDto, { new: true });
+      return pokemon;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  handleExceptions(error) {
+    if ((error.code = MONGO_ERROR.DUPLICATE_KEY)) {
+      throw new BadRequestException(
+        `Pokemon exists in DB ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+
+    console.log(error);
+
+    throw new InternalServerErrorException(
+      `Can't update Pokemon - Check server logs`,
+    );
+  }
+
+  async remove(id: string) {
+    // const pokemon = await this.findOne(term);
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+
+    if (!deletedCount) throw new NotFoundException(`'${id}' not found`);
+    return;
+  }
+}
